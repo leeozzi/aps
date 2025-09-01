0e0ecc2f137f83b4f9fa2240e6780aff37000b95
이언지 <lstory1997l@gmail.com>
2025-09-01 17:55:09 +0900

    250901

--- DoIt알고리즘코딩테스트자바편/P004_구간합구하기2_BOJ11660.java
+++ DoIt알고리즘코딩테스트자바편/P004_구간합구하기2_BOJ11660.java
@@ -1,4 +1,19 @@
 package DoIt알고리즘코딩테스트자바편;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class P004_구간합구하기2_BOJ11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] orgMap = new int[N][N];
+        int[][] sumMap = new int[N][N];
+
+
+    }
 }
